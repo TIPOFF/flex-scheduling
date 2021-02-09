add18e28d4254b7d7741c5780be5a363b089a386
Beau Beauchamp, WebTigers <beau@webtigers.com>
2021-02-09 17:40:18 -0500

    Load Migrations from Package - removed unnecessary function

--- src/FlexDayServiceProvider.php
+++ src/FlexDayServiceProvider.php
@@ -28,19 +28,4 @@ class FlexDayServiceProvider extends PackageServiceProvider
             ->name('flex-scheduling')
             ->hasConfigFile();
     }
-
-    /**
-     * Using packageBooted lifecycle hooks to override the migration file name.
-     * We want to keep the old filename for now.
-     */
-    public function packageBooted()
-    {
-        foreach ($this->package->migrationFileNames as $migrationFileName) {
-            if (! $this->migrationFileExists($migrationFileName)) {
-                $this->publishes([
-                    $this->package->basePath("/../database/migrations/{$migrationFileName}.php.stub") => database_path('migrations/' . Str::finish($migrationFileName, '.php')),
-                ], "{$this->package->name}-migrations");
-            }
-        }
-    }
 }
