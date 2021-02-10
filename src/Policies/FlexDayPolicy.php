467fa6df124452fc79c08ef8c63e267dd9a54b02
Chris H <crishack8@gmail.com>
2021-02-09 23:12:13 -0600

    Add Policies

---
+++ src/Policies/FlexDayPolicy.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Tipoff\FlexScheduling\Policies;
+
+use Tipoff\FlexScheduling\Models\FlexDay;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class FlexDayPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        return $user->hasPermissionTo('view flex days') ? true : false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\FlexDay  $flexDay
+     * @return mixed
+     */
+    public function view(User $user, FlexDay $flexDay)
+    {
+        return $user->hasPermissionTo('view flex days') ? true : false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\FlexDay  $flexDay
+     * @return mixed
+     */
+    public function update(User $user, FlexDay $flexDay)
+    {
+        return $user->hasPermissionTo('update flex days') ? true : false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\FlexDay  $flexDay
+     * @return mixed
+     */
+    public function delete(User $user, FlexDay $flexDay)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\FlexDay  $flexDay
+     * @return mixed
+     */
+    public function restore(User $user, FlexDay $flexDay)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\FlexDay  $flexDay
+     * @return mixed
+     */
+    public function forceDelete(User $user, FlexDay $flexDay)
+    {
+        return false;
+    }
+}
